b18d40385fef69b8b4f4e4cc9401cde4da538277
unknown <jeffinpoulose2@gmail.com>
2020-06-19 20:15:00 +0530

    End of Section 5 Error

--- DattingApp.API/Controllers/AuthController.cs
+++ DattingApp.API/Controllers/AuthController.cs
@@ -51,6 +51,8 @@ namespace DattingApp.API.Controllers
 
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
