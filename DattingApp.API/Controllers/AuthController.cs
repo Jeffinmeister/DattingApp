61b8828be96846a720305a3a74674ec9171440af
unknown <jeffinpoulose2@gmail.com>
2020-06-21 01:12:43 +0530

    End of Section 6 3rd party

--- DattingApp.API/Controllers/AuthController.cs
+++ DattingApp.API/Controllers/AuthController.cs
@@ -52,7 +52,6 @@ namespace DattingApp.API.Controllers
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
 
-
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
