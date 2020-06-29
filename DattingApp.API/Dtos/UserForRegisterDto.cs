417c7b8fe6d9082c54552e2f24100037094e8512
unknown <jeffinpoulose2@gmail.com>
2020-06-29 09:49:53 +0530

    End of Module12-Reactive Forms

--- DattingApp.API/Dtos/UserForRegisterDto.cs
+++ DattingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DattingApp.API.Dtos
@@ -11,5 +12,26 @@ namespace DattingApp.API.Dtos
         [Required]
         [StringLength(8, MinimumLength=4, ErrorMessage = "You must have a password between 8 to 14")]    
         public string Password { get; set; }
+        
+        [Required]
+        public string KnownAs { get; set; }
+        
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+        
+        [Required]
+        public string Country { get; set; }
+        
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
